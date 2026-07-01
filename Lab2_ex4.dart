fdd91b4e507d9ea090b19cb69c48ab8f3b9c1ee4
Prince0o0 <silichot63@gmail.com>
2026-07-01 10:50:19 +0700

    NullSafety

---
+++ Lab2_ex4.dart
@@ -0,0 +1,20 @@
+void main(){
+  String? middleName;
+  String city = 'Nakhon Sawan';
+
+  int middleNameLength = middleName?.length ?? 0;
+  print('Middle name length: $middleNameLength');
+  
+  print('City: ${city.length}');
+
+  print('MiddleName be for assignment: $middleName');
+
+
+  int cityLength = city.length;
+  print('City length: $cityLength');
+
+  middleName ??= 'Somethingelse';
+  print('MiddleName after assignment: $middleName');
+
+
+}
